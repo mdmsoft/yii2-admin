667ee42ca50d08f67013fea48d795af7572fcd4b
mdmunir <misbahuldmunir@gmail.com>
2014-07-17 10:18:06 +0700

    Add option for mysql

--- migrations/m140602_111327_create_menu_table.php
+++ migrations/m140602_111327_create_menu_table.php
@@ -7,6 +7,11 @@ class m140602_111327_create_menu_table extends \yii\db\Migration
 
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('{{%menu}}', [
             'id' => Schema::TYPE_PK,
             'name' => Schema::TYPE_STRING . '(128) NOT NULL',
@@ -15,7 +20,7 @@ class m140602_111327_create_menu_table extends \yii\db\Migration
             'order' => Schema::TYPE_INTEGER,
             'data' => Schema::TYPE_TEXT,
             'FOREIGN KEY (parent) REFERENCES {{%menu}}(id) ON DELETE SET NULL ON UPDATE CASCADE',
-        ]);
+        ], $tableOptions);
     }
 
     public function safeDown()
