849565c30b56f0d950615aa58b11854adab5a229
marsuboss <marsuboss@gmail.com>
2014-11-05 10:00:17 +0100

    Translate French

---
+++ messages/fr/rbac-admin.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Message translations for \mdmsoft\yii2-admin.
+ *
+ * It contains the localizable messages extracted from source code.
+ * You may modify this file by translating the extracted messages.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Assignments' => 'Assignements',
+    'Users' => 'Utilisateurs',
+    'User' => 'Utilisateur',
+    'Avaliable' => 'Disponible',
+    'Assigned' => 'Assigner',
+    'Create' => 'Créer',
+    'Update' => 'Modifier',
+    'Roles' => 'Rôles',
+    'Create Role' => 'Créer Rôle',
+    'Name' => 'Nom',
+    'Type' => 'Type',
+    'Description' => 'Description',
+    'Rule Name' => 'Nom de la règle',
+    'Data' => 'Données',
+    'Update Role' => 'Modifier Rôle',
+    'Delete' => 'Supprimer',
+    'Are you sure to delete this item?' => 'Etes-vous sûr de vouloir supprimer cet item ?',
+    'ID' => 'ID',
+    'Parent' => 'Parent',
+    'Parent Name' => 'Nom Parent',
+    'Route' => 'Route',
+    'Username' => 'Nom d\'utilisateur',
+    'Update Permission' => 'Modifier Autorisation',
+    'Permissions' => 'Autorisations',
+    'Permission' => 'Autorisation',
+    'Create Permission' => 'Créer Autorisation',
+    'Create Permissions' => 'Créer Autorisations',
+    'Routes' => 'Routes',
+    'Create route' => 'Créer route',
+    'New' => 'Nouveau',
+    'Generate Routes' => 'Générer Routes',
+    'Append' => 'Ajouter',
+    'Create Rule' => 'Créer Règle',
+    'Rules' => 'Règles',
+    'Update Rule' => 'Modifier Règle',
+    'Create Menu' => 'Créer Menu',
+    'Menus' => 'Menus',
+    'Search' => 'Rechercher',
+    'Reset' => 'Réinitialiser',
+    'Update Menu' => 'Modifier Menu',
+    'Menus' => 'Menus',
+    'Order' => 'Ordre',
+    'Class Name' => 'Nom de la Classe',
+    'Assignment' => 'Assignement',
+    'Role' => 'Rôle',
+    'Rule' => 'Règle',
+    'Menu' => 'Menu',
+    'Help' => 'Aide',
+    'Application' => 'Application',
+];
