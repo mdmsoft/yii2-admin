401c537ad962cacd0af76608389fbab63de50a64
mdmunir <misbahuldmunir@gmail.com>
2014-12-13 21:44:47 +0700

    fixed #95, #100

--- Module.php
+++ Module.php
@@ -38,7 +38,6 @@ use yii\helpers\Inflector;
  */
 class Module extends \yii\base\Module
 {
-
     /**
      * @inheritdoc
      */
@@ -48,8 +47,27 @@ class Module extends \yii\base\Module
      * @var array 
      * @see [[items]]
      */
-    private $_menus;
-    
+    private $_menus = [];
+
+    /**
+     * @var array 
+     * @see [[items]]
+     */
+    private $_coreItems = [
+        'assignment' => 'Assignments',
+        'role' => 'Roles',
+        'permission' => 'Permissions',
+        'route' => 'Routes',
+        'rule' => 'Rules',
+        'menu' => 'Menus',
+    ];
+
+    /**
+     * @var array 
+     * @see [[items]]
+     */
+    private $_normalizeMenus;
+
     /**
      * Nav bar items
      * @var array  
@@ -60,22 +78,21 @@ class Module extends \yii\base\Module
      * @var string Main layout using for module. Default to layout of parent module.
      * Its used when `layout` set to 'left-menu', 'right-menu' or 'top-menu'.
      */
-    public $mainLayout ='@mdm/admin/views/layouts/main.php';
+    public $mainLayout = '@mdm/admin/views/layouts/main.php';
 
     /**
      * @inheritdoc
      */
-	public function init()
+    public function init()
     {
         parent::init();
         Yii::$app->i18n->translations['rbac-admin'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en',
             'basePath' => '@mdm/admin/messages'
-            
         ];
         //user did not define the Navbar?
-        if($this->navbar === null){        
+        if ($this->navbar === null) {
             $this->navbar = [
                 ['label' => Yii::t('rbac-admin', 'Help'), 'url' => 'https://github.com/mdmsoft/yii2-admin/blob/master/docs/guide/basic-usage.md'],
                 ['label' => Yii::t('rbac-admin', 'Application'), 'url' => Yii::$app->homeUrl]
@@ -84,37 +101,43 @@ class Module extends \yii\base\Module
     }
 
     /**
-     * Get core menu
-     * @return array
-     * @var $ids array has 'Menu Lable' => 'Controller' pairs
-     */
-    protected function getCoreMenus()
-    {
-        $mid = '/' . $this->getUniqueId() . '/';
-        $ids = ['Assignments' => 'assignment', 'Roles' => 'role', 'Permissions' => 'permission', 'Routes' => 'route', 'Rules' => 'rule', 'Menus' => 'menu'];
-        $config = components\Configs::instance();
-        $result = [];
-        foreach ($ids as $lable => $id) {
-            if ($id !== 'menu' || ($config->db !== null && $config->db->schema->getTableSchema($config->menuTable) !== null)) {
-                $result[$id] = ['label' => Yii::t('rbac-admin', $lable), 'url' => [$mid . $id]];
-            }
-        }
-        foreach (array_keys($this->controllerMap) as $id) {
-            $result[$id] = ['label' => Yii::t('rbac-admin', Inflector::humanize($id)), 'url' => [$mid . $id]];
-        }
-        return $result;
-    }
-
-    /**
      * Get avalible menu.
      * @return array
      */
     public function getMenus()
     {
-        if ($this->_menus === null) {
-            return $this->_menus = $this->getCoreMenus();
+        if ($this->_normalizeMenus === null) {
+            $mid = '/' . $this->getUniqueId() . '/';
+            // resolve core menus
+            $this->_normalizeMenus = [];
+            $config = components\Configs::instance();
+            foreach ($this->_coreItems as $id => $lable) {
+                if ($id !== 'menu' || ($config->db !== null && $config->db->schema->getTableSchema($config->menuTable) !== null)) {
+                    $this->_normalizeMenus[$id] = ['label' => Yii::t('rbac-admin', $lable), 'url' => [$mid . $id]];
+                }
+            }
+            foreach (array_keys($this->controllerMap) as $id) {
+                $this->_normalizeMenus[$id] = ['label' => Yii::t('rbac-admin', Inflector::humanize($id)), 'url' => [$mid . $id]];
+            }
+
+            // user configure menus
+            foreach ($this->_menus as $id => $value) {
+                if (empty($value)) {
+                    unset($this->_normalizeMenus[$id]);
+                } else {
+                    if (is_string($value)) {
+                        $value = [
+                            'label' => $value,
+                        ];
+                    }
+                    $this->_normalizeMenus[$id] = isset($this->_normalizeMenus[$id]) ? array_merge($this->_normalizeMenus[$id], $value) : $value;
+                    if (!isset($this->_normalizeMenus[$id]['url'])) {
+                        $this->_normalizeMenus[$id]['url'] = [$mid . $id];
+                    }
+                }
+            }
         }
-        return $this->_menus;
+        return $this->_normalizeMenus;
     }
 
     /**
@@ -123,17 +146,7 @@ class Module extends \yii\base\Module
      */
     public function setMenus($menus)
     {
-        $mid = '/' . $this->getUniqueId() . '/';
-        $this->_menus = $this->getMenus();
-        foreach ($menus as $id => $value) {
-            if (empty($value)) {
-                unset($this->_menus[$id]);
-            } else {
-                $this->_menus[$id] = isset($this->_menus[$id]) ? array_merge($this->_menus[$id], $value) : $value;
-                if (!isset($this->_menus[$id]['url'])) {
-                    $this->_menus[$id]['url'] = [$mid . $id];
-                }
-            }
-        }
+        $this->_menus = array_merge($this->_menus, $menus);
+        $this->_normalizeMenus = null;
     }
 }
