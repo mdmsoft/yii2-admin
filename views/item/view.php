765b1592f61b7df33add9a3afcf8d4221d0e3a75
apexwire <apexwire@gmail.com>
2017-12-18 20:31:32 +0300

    Update item select and others:
    - AssignmentController. Add comment. Remove duplicate lines: 'assign' => ['post'].
    - Assignment and AuthItem. Update getItems(). getItems() return array with parametr 'description' to display the list of items
    - in ./views/assignnets/view.php and ./views/item/view.php add  $labels  - localization support js. Update _script.js too.

--- views/item/view.php
+++ views/item/view.php
@@ -21,59 +21,65 @@ YiiAsset::register($this);
 $opts = Json::htmlEncode([
     'items' => $model->getItems(),
 ]);
+$labels = Json::htmlEncode([
+    'Roles' => Yii::t('rbac-admin', 'Roles'),
+    'Permissions' => Yii::t('rbac-admin', 'Permissions'),
+    'Routes' => Yii::t('rbac-admin', 'Routes'),
+]);
 $this->registerJs("var _opts = {$opts};");
+$this->registerJs("var _labels = {$labels};");
 $this->registerJs($this->render('_script.js'));
 $animateIcon = ' <i class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></i>';
 ?>
 <div class="auth-item-view">
-    <h1><?=Html::encode($this->title);?></h1>
+    <h1><?= Html::encode($this->title); ?></h1>
     <p>
-        <?=Html::a(Yii::t('rbac-admin', 'Update'), ['update', 'id' => $model->name], ['class' => 'btn btn-primary']);?>
-        <?=Html::a(Yii::t('rbac-admin', 'Delete'), ['delete', 'id' => $model->name], [
-    'class' => 'btn btn-danger',
-    'data-confirm' => Yii::t('rbac-admin', 'Are you sure to delete this item?'),
-    'data-method' => 'post',
-]);?>
-        <?=Html::a(Yii::t('rbac-admin', 'Create'), ['create'], ['class' => 'btn btn-success']);?>
+        <?= Html::a(Yii::t('rbac-admin', 'Update'), ['update', 'id' => $model->name], ['class' => 'btn btn-primary']); ?>
+        <?= Html::a(Yii::t('rbac-admin', 'Delete'), ['delete', 'id' => $model->name], [
+            'class' => 'btn btn-danger',
+            'data-confirm' => Yii::t('rbac-admin', 'Are you sure to delete this item?'),
+            'data-method' => 'post',
+        ]); ?>
+        <?= Html::a(Yii::t('rbac-admin', 'Create'), ['create'], ['class' => 'btn btn-success']); ?>
     </p>
     <div class="row">
         <div class="col-sm-11">
             <?=
-DetailView::widget([
-    'model' => $model,
-    'attributes' => [
-        'name',
-        'description:ntext',
-        'ruleName',
-        'data:ntext',
-    ],
-    'template' => '<tr><th style="width:25%">{label}</th><td>{value}</td></tr>',
-]);
-?>
+            DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'name',
+                    'description:ntext',
+                    'ruleName',
+                    'data:ntext',
+                ],
+                'template' => '<tr><th style="width:25%">{label}</th><td>{value}</td></tr>',
+            ]);
+            ?>
         </div>
     </div>
     <div class="row">
         <div class="col-sm-5">
             <input class="form-control search" data-target="available"
-                   placeholder="<?=Yii::t('rbac-admin', 'Search for available');?>">
+                   placeholder="<?= Yii::t('rbac-admin', 'Search for available'); ?>">
             <select multiple size="20" class="form-control list" data-target="available"></select>
         </div>
         <div class="col-sm-1">
             <br><br>
-            <?=Html::a('&gt;&gt;' . $animateIcon, ['assign', 'id' => $model->name], [
-    'class' => 'btn btn-success btn-assign',
-    'data-target' => 'available',
-    'title' => Yii::t('rbac-admin', 'Assign'),
-]);?><br><br>
-            <?=Html::a('&lt;&lt;' . $animateIcon, ['remove', 'id' => $model->name], [
-    'class' => 'btn btn-danger btn-assign',
-    'data-target' => 'assigned',
-    'title' => Yii::t('rbac-admin', 'Remove'),
-]);?>
+            <?= Html::a('&gt;&gt;' . $animateIcon, ['assign', 'id' => $model->name], [
+                'class' => 'btn btn-success btn-assign',
+                'data-target' => 'available',
+                'title' => Yii::t('rbac-admin', 'Assign'),
+            ]); ?><br><br>
+            <?= Html::a('&lt;&lt;' . $animateIcon, ['remove', 'id' => $model->name], [
+                'class' => 'btn btn-danger btn-assign',
+                'data-target' => 'assigned',
+                'title' => Yii::t('rbac-admin', 'Remove'),
+            ]); ?>
         </div>
         <div class="col-sm-5">
             <input class="form-control search" data-target="assigned"
-                   placeholder="<?=Yii::t('rbac-admin', 'Search for assigned');?>">
+                   placeholder="<?= Yii::t('rbac-admin', 'Search for assigned'); ?>">
             <select multiple size="20" class="form-control list" data-target="assigned"></select>
         </div>
     </div>
